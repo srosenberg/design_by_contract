ec4031a75d11d314342529fe928195a224c1f311
Stan Rosenberg <srosenberg@forensiq.com>
2017-12-05 13:46:27 -0500

    Add quickcheck tests

--- src/main/java/com/placeiq/Samples.java
+++ src/main/java/com/placeiq/Samples.java
@@ -14,7 +14,8 @@ public class Samples {
     @Requires({ " a != null" })
 	@Ensures({ "Samples.isReverseOf(result, a)" })
 	public static<T> T[] reverse(T[] a, Class<T> elemType) {
-		T[] result = (T[]) Array.newInstance(elemType, a.length);
+		@SuppressWarnings("unchecked")
+        T[] result = (T[]) Array.newInstance(elemType, a.length);
 		System.arraycopy(a, 0, result, 0, a.length);
 		
 		int mid = result.length / 2;
@@ -26,6 +27,9 @@ public class Samples {
 		return result;
 	}
 	
+    //@Requires( { "a != null && 0 <= i && i < a.length && 0 <=j && j < a.length" })
+    //@Ensures( { "a[i] == old(a[j]) && a[j] == old(a[i])" })
+    //TODO: need a way to specify universal quantification, i.e., forall k :: k != i && k != j ==> old(a[k]) == a[k]
 	public final static<T> void swap(T[] a, int i, int j) {
 		T tmp = a[i];
 		
@@ -42,5 +46,34 @@ public class Samples {
 		Collections.reverse(aReversed);
 		
 		return a.length == b.length && aReversed.equals(Arrays.asList(b));
+	}
+	
+	@Requires({ " a != null && b != null" })
+	@Ensures({ "result != null && Arrays.equals(old(a), a) && Arrays.equals(old(b), b)" })
+	public static int[] intersection(int[] a, int[] b) {
+		int[] result = new int[Math.min(a.length, b.length)];
+		
+		if (result.length == 0) {
+			return result;
+		}
+		Arrays.sort(a);
+		Arrays.sort(b);
+		
+		int i, j, k;
+		
+		i = j = k = 0;
+		
+		while  (i < a.length && j < b.length) {
+			if (a[i] < b[j]) {
+				i++;
+			} else if (a[i] > b[j]) {
+				j++;
+			} else {
+				result[k++] = a[i];
+				i++;
+				j++;
+			}
+		}
+		return Arrays.copyOf(result, k);
 	}
 }
--- src/test/java/com/placeiq/SamplesTest.java
+++ src/test/java/com/placeiq/SamplesTest.java
@@ -1,27 +1,72 @@
 package com.placeiq;
 
+import static org.hamcrest.Matchers.isIn;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeTrue;
+
 import java.util.Random;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
 
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.generator.InRange;
+
+@RunWith(Theories.class)
 public class SamplesTest {
 	
 	@Test
-	public void testBanal() {
+	// lame unit test
+	public void testReverseFixedInputs() {
 		Assert.assertArrayEquals(toArray("moob"), Samples.reverse(toArray("boom"), Character.class));
 		
 		Assert.assertArrayEquals(toArray("bob"), Samples.reverse(toArray("bob"), Character.class));
 	}
 	
 	@Test
-	public void testRandom() {
+	// randomized testing (manual input generation) 
+	public void testReverseRandomInputs() {
 		for (int i = 0; i < 100; i++) {
 			String s = getRandom();
 			System.out.println("original: " + s);
 			System.out.println("reversed: " + StringUtils.join(Samples.reverse(toArray(s), Character.class)));
+		}
+	}
+	
+	@Theory
+	// randomized testing ala quickcheck
+	public void testReverseQC(@ForAll String s) {
+		assumeTrue(s.length() > 2);
+
+		System.out.println("original: " + s);
+		System.out.println("reversed: " + StringUtils.join(Samples.reverse(toArray(s), Character.class)));
+	}
+	
+	@Theory
+	public void testIntersection(@ForAll(sampleSize=10) @InRange(minInt = 0, maxInt = 20) int[] a, 
+			                     @ForAll(sampleSize=10) @InRange(minInt = 0, maxInt = 20) int[] b) {
+		assumeTrue(a != null);
+		assumeTrue(b != null);
+		
+		System.out.println("a: " + StringUtils.join(ArrayUtils.toObject(a), ","));
+		System.out.println("b: " + StringUtils.join(ArrayUtils.toObject(b), ","));
+		
+		int[] result = Samples.intersection(a, b);
+		
+		System.out.println("result: " + StringUtils.join(ArrayUtils.toObject(result), ","));
+
+		// partial correctness condition (wish there were a way to write quantifiers...)
+		for (int x : a) {
+			for (int y : b) {
+				if (x == y) {
+					assertThat(x, isIn(ArrayUtils.toObject(result)));
+				}
+			}
 		}
 	}
 	
